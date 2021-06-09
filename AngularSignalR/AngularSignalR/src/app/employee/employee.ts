f912ae7d64e727aabe11a39e0447ff56a6cf9132
Svetoslav Pehlivanov <Svetoslav.Pehlivanov.consultant@cconsulting.it>
2021-06-09 20:56:48 +0300

    new comp

---
+++ AngularSignalR/AngularSignalR/src/app/employee/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+  id: string | null,
+  name: String,
+  address: string | null | undefined,
+  gender: string | null | undefined,
+  company: string | null | undefined,
+  designation: string | null | undefined,
+  cityname: string | null | undefined
+}
